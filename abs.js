b6c5e862f68f45aee936b0e7e3fd1b4bfd9ea605
01Buks-Mark <markbukenya04@gmail.com>
2024-06-30 19:36:34 +0300

    Updated the FaQs page

--- abs.js
+++
@@ -1,8 +0,0 @@
-const slideshow = document.querySelector('.slideshow');
-const images = slideshow.querySelectorAll('img');
-let currentImage = 0;
-setInterval(() => {
-  images[currentImage].classList.remove('show');
-  currentImage = (currentImage + 1) % images.length;
-  images[currentImage].classList.add('show');
-}, 3000);
